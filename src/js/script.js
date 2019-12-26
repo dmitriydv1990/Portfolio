471925cb041e0528767bfef577afec57f69d987c
dmitriydv1990 <dmitriydv1990@gmail.com>
2019-12-26 16:37:32 +0600

    arrow up and script jquery

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
 new WOW().init();
+
+
+/* функция плавной прокрутки при нажатии на ссылки в меню и стрелки вверх */
+
+$("a[href^='#']").click(function(){
+    var _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
+
+/* функция стрелки вверх */
+
+$(window).scroll(function() {
+	if ($(this).scrollTop() > 250) {
+		$('.arrow-up').fadeIn();
+	} else {
+		$('.arrow-up').fadeOut();
+	}
+
+});
